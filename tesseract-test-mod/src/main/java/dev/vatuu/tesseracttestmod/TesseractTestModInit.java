6a24c1099d6fe72d120c8bbce21128938f8d2ba7
PheonixVX <24632178+PhoenixVX@users.noreply.github.com>
2021-07-08 20:00:14 -0400

    Submodules?!?

---
+++ tesseract-test-mod/src/main/java/dev/vatuu/tesseracttestmod/TesseractTestModInit.java
@@ -0,0 +1,10 @@
+package dev.vatuu.tesseracttestmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TesseractTestModInit implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
